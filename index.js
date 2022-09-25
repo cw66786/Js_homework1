0b7eb01dd004c327d05a74e14553359a529dab31
cw66786 <103803383+cw66786@users.noreply.github.com>
2022-09-25 01:50:09 -0500

    1 file modified

--- index.js
+++ index.js
@@ -429,11 +429,12 @@ console.log("23:",firstNonRepeat('abacddbec'))
 let bubbleEx = [12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213];
 
 const bubbleSort = (arr)=>{
-  for(let i = 0;i<arr.length;i++){
+  for(let i = 0;i<arr.length-1;i++){
     for(let j =0;j<arr.length-1;j++){
-      if(arr[j]>arr[j+1]){
+      if(arr[j] < arr[j+1]){
+        let temp = arr[j];
         arr[j] = arr[j+1];
-        arr[j+1] = arr[j];
+        arr[j+1] = temp;
       }
     }
 
